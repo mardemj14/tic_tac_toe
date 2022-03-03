8137223f951cddb03ffc8b28ddb806473ed666d7
Demar Jones <96274159+mardemj@users.noreply.github.com>
2022-03-03 08:37:02 -0600

    Add logic to check for round win

--- app.js
+++ app.js
@@ -7,9 +7,46 @@ const Player = (sign) => {
 };
 
 const GameBoard = (() => {
-  const squares = Array.from(document.querySelectorAll('.square'));
+  const playerX = Player('X');
+  const playerO = Player('O');
 
-  return { squares };
+  let activePlayer = playerX;
+  let roundWin = false;
+
+  const squares = Array.from(document.querySelectorAll('.square'));
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === playerX ? playerO : playerX;
+    return activePlayer;
+  };
+
+  const checkWin = () => {
+    winConditions.forEach((condition) => {
+      // first, second and third index
+      const [f, s, t] = condition;
+
+      if (
+        squares[f].textContent === squares[s].textContent &&
+        squares[s].textContent === squares[t].textContent &&
+        squares[f].textContent !== ''
+      ) {
+        roundWin = true;
+        console.log(`${activePlayer.sign} Won!`);
+      }
+    });
+  };
+
+  return { squares, switchPlayer, activePlayer, checkWin };
 })();
 
 const DisplayController = (() => {
@@ -17,21 +54,20 @@ const DisplayController = (() => {
     squares.forEach((square) => {
       square.addEventListener('click', () => {
         // check if square is vacant before updating
-        if (square.textContent === '') square.textContent = player.sign;
-        else console.log('Square is occupied');
+        if (square.textContent === '') {
+          square.textContent = player.sign;
+        } else console.log('Square is occupied');
+
+        // check for round win / draw
+        GameBoard.checkWin();
+
+        // switch active player after each turn
+        player = GameBoard.switchPlayer();
       });
     });
   };
 
-  const renderPage = (gameboard) => {
-    gameboard.forEach((square) => {
-      square.textContent = 'X';
-    });
-  };
-
-  return { renderPage, updateSquareOnClick };
+  return { updateSquareOnClick };
 })();
 
-const playerX = Player('X');
-
-DisplayController.updateSquareOnClick(GameBoard.squares, playerX);
+DisplayController.updateSquareOnClick(GameBoard.squares, GameBoard.activePlayer);
